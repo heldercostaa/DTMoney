7e7701ac303e71408c37e3427d10c91a15f1564a
Helder Costa <heldercostaaa@gmail.com>
2021-07-13 20:50:42 -0300

    Style income and outcome table

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,7 +8,7 @@ export const GlobalStyles = createGlobalStyle`
     
     --blue-light: #6933ff;
     
-    --text-tile: #363f5f;
+    --text-title: #363f5f;
     --text-body: #969cb3;
     
     --background: #f0f2f5;
