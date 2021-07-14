eb68fffa29ef82fbe3af9b215358045ba5d1e73d
Helder Costa <heldercostaaa@gmail.com>
2021-07-14 20:37:29 -0300

    Add state to  select income or outcome buttons; Style a bit more them as well

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import Modal from "react-modal";
 
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -15,6 +16,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+
   return (
     <Modal
       isOpen={isOpen}
@@ -34,18 +37,31 @@ export function NewTransactionModal({
         <h2>Create Transaction</h2>
 
         <input placeholder="Title" />
+
         <input type="number" placeholder="Value" />
+
         <TransactionTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+          >
             <img src={incomeImg} alt="Income" />
             <span>Income</span>
-          </button>
-          <button type="button">
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+          >
             <img src={outcomeImg} alt="Outcome" />
             <span>Outcome</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
+
         <input placeholder="Category" />
+
         <button type="submit">Create</button>
       </Container>
     </Modal>
