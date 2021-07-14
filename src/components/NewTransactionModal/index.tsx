dfc31e72b57b7e7078f2d5b30a807d52758f2bc5
Helder Costa <heldercostaaa@gmail.com>
2021-07-14 20:19:23 -0300

    Finish modal inputs, button and close button styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
 import { Container } from "./styles";
 
 type NewTransactionModalProps = {
@@ -17,6 +18,14 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close modal" />
+      </button>
+
       <Container>
         <h2>Create Transaction</h2>
 
