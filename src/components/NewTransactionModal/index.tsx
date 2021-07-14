5b92429e7592ba7109ff7f118856045194dbbf2f
Helder Costa <heldercostaaa@gmail.com>
2021-07-14 20:09:59 -0300

    Start to style modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -10,8 +11,20 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Create Transaction</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Create Transaction</h2>
+
+        <input placeholder="Title" />
+        <input type="number" placeholder="Value" />
+        <input placeholder="Category" />
+        <button type="submit">Create</button>
+      </Container>
     </Modal>
   );
 }
