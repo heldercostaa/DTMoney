a27c007cc0199974337c6921e7d0c7cc9877ca80
Helder Costa <heldercostaaa@gmail.com>
2021-07-14 21:08:16 -0300

    List initial transactions

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Website freelance",
+          type: "deposit",
+          category: "Dev",
+          amount: "6000",
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Apartment rent",
+          type: "withdraw",
+          category: "House",
+          amount: "1100",
+          createdAt: new Date("2021-02-14 09:02:34"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
