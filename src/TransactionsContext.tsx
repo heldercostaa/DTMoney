6f3ef9b440a7d7f1f9b016c9cb613d7d25ad6431
Helder Costa <heldercostaaa@gmail.com>
2021-07-15 21:37:58 -0300

    Move create transaction code to context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -11,11 +11,27 @@ type Transaction = {
   createdAt: string;
 };
 
+// type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+// type TransactionInput = Pick<Transaction, "title" | 'amount' | 'type' | 'category'>;
+type TransactionInput = {
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+};
+
 type TransactionsProviderProps = {
   children: ReactNode;
 };
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+type TransactionsContextData = {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+};
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -26,8 +42,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then((response) => setTransactions(response.data.transactions));
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post("/transactions", transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   );
