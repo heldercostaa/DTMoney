04e0e15d1b059fb940be030907bf7707f82527d6
Helder Costa <heldercostaaa@gmail.com>
2021-07-15 21:58:32 -0300

    Reflect transaction values in summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -11,7 +11,41 @@ import { Container } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions);
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += +transaction.amount;
+        acc.total += +transaction.amount;
+      } else {
+        acc.withdraws += +transaction.amount;
+        acc.total -= +transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
+  const locale = Intl.DateTimeFormat().resolvedOptions().timeZoneName;
+
+  const deposits = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: "USD",
+  }).format(summary.deposits);
+
+  const withdraws = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: "USD",
+  }).format(summary.withdraws);
+
+  const total = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: "USD",
+  }).format(summary.total);
 
   return (
     <Container>
@@ -20,21 +54,21 @@ export function Summary() {
           <p>Income</p>
           <img src={incomeImg} alt="Incomes" />
         </header>
-        <strong>$1000.00</strong>
+        <strong>{deposits}</strong>
       </div>
       <div>
         <header>
           <p>Outcome</p>
           <img src={outcomeImg} alt="Outcomes" />
         </header>
-        <strong>$500.00</strong>
+        <strong>- {withdraws}</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>$500.00</strong>
+        <strong>{total}</strong>
       </div>
     </Container>
   );
